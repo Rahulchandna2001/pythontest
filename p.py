c16c29858f171bd63a24714c8e90d14c6e298e31
Rahulchandna2001 <95170585+Rahulchandna2001@users.noreply.github.com>
2024-09-23 16:26:18 -0400

    V1

---
+++ p.py
@@ -0,0 +1,11 @@
+# Basic data types
+integer_example = 10
+float_example = 10.5
+string_example = "Database Automation"
+boolean_example = True
+
+# Printing the types of variables
+print("Integer:", type(integer_example))
+print("Float:", type(float_example))
+print("String:", type(string_example))
+print("Boolean:", type(boolean_example))
